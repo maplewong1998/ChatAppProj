42ea82bb02d3d5c6f10832ae0d40bfd433b575d3
maplewong1998 <maplewong1998@gmail.com>
2020-07-15 02:15:19 +0800

    Second commit

--- ChatServer/src/com/cs/ServerWorker.java
+++ ChatServer/src/com/cs/ServerWorker.java
@@ -4,15 +4,14 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.HashSet;
 import java.util.List;
+
 
 public class ServerWorker extends Thread {
     private final Socket clientSocket;
     private final Server server;
     private String username = null;
     private OutputStream outputStream;
-    private HashSet<String> chatRooms = new HashSet<>();
 
     public ServerWorker (Server server, Socket clientSocket) {
         this.server = server;
@@ -25,12 +24,10 @@ public class ServerWorker extends Thread {
             ClientSocketHandler();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException exception) {
-            exception.printStackTrace();
         }
     }
 
-    private void ClientSocketHandler() throws IOException, InterruptedException {
+    private void ClientSocketHandler() throws IOException {
         InputStream inputStream = clientSocket.getInputStream();
         this.outputStream = clientSocket.getOutputStream();
 
@@ -40,7 +37,7 @@ public class ServerWorker extends Thread {
             String[] tokens = StringUtils.split(line);
             if (tokens != null && tokens.length > 0) {
                 String cmd = tokens[0];
-                if ("quit".equalsIgnoreCase(cmd) || "logout".equalsIgnoreCase(cmd)) {
+                if ("logout".equalsIgnoreCase(cmd)) {
                     LogoutHandler();
                     break;
                 } else if ("login".equalsIgnoreCase(cmd)) {
@@ -48,11 +45,7 @@ public class ServerWorker extends Thread {
                 } else if ("msg".equalsIgnoreCase(cmd)) {
                     String[] msgTokens = StringUtils.split(line, null, 3);
                     MessageHandler(msgTokens);
-                } else if ("join".equalsIgnoreCase(cmd)) {
-                    JoinHandler(tokens);
-                } else if ("leave".equalsIgnoreCase(cmd)) {
-                    LeaveHandler(tokens);
-                }else {
+                } else {
                     String msg = "unknown command " + cmd + "\n";
                     outputStream.write(msg.getBytes());
                 }
@@ -60,50 +53,15 @@ public class ServerWorker extends Thread {
         }
     }
 
-    public boolean isChatRoom(String room) {
-        return room.charAt(0) == '#';
-    }
-
-    public boolean isMemberOfChatRoom(String room) {
-        return chatRooms.contains(room);
-    }
-
-    private void LeaveHandler(String[] tokens) {
-        if (tokens.length > 1) {
-            String room = tokens[1];
-            if(isChatRoom(room)) {
-                chatRooms.remove(room);
-            }
-        }
-    }
-
-    private void JoinHandler(String[] tokens) {
-        if (tokens.length > 1) {
-            String room = tokens[1];
-            if(isChatRoom(room)) {
-                chatRooms.add(room);
-            }
-        }
-    }
-
-    // direct chat: "msg" "username" msgBody
-    // room chat: "msg" "#chatRoom" msgBody
     private void MessageHandler(String[] tokens) throws IOException {
         String sendTo = tokens[1];
         String msgBody = tokens[2];
 
         List<ServerWorker> workers = server.getWorkers();
         for(ServerWorker worker : workers) {
-            if (isChatRoom(sendTo)) {
-                if (worker.isMemberOfChatRoom(sendTo)) {
-                    String outMsg = "msg " + sendTo + " " + username + " " + msgBody + "\n";
-                    worker.send(outMsg);
-                }
-            } else {
-                if (sendTo.equalsIgnoreCase(worker.getUsername())) {
-                    String outMsg = "msg " + username + " " + msgBody + "\n";
-                    worker.send(outMsg);
-                }
+            if (sendTo.equalsIgnoreCase(worker.getUsername())) {
+                String outMsg = "msg " + username + " " + msgBody + "\n";
+                worker.send(outMsg);
             }
         }
     }
@@ -117,6 +75,7 @@ public class ServerWorker extends Thread {
             String username = tokens[1];
             String password = tokens[2];
 
+            //hardcoded accounts: admin and guest
             if ((username.equals("admin") && password.equals("admin")) || (username.equals("guest") && password.equals("guest"))) {
                 String msg = "Login successful\n";
                 outputStream.write(msg.getBytes());
@@ -133,6 +92,7 @@ public class ServerWorker extends Thread {
                         }
                     }
                 }
+
                 //notify all other online users new login
                 String BroadcastMsg = "online " + getUsername() + "\n";
                 for(ServerWorker worker : workers) {
